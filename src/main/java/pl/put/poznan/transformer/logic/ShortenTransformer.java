32f2cbb8b00495881b0ea43becb9284b146f64e9
KcprZtn <kcprzaton@gmail.com>
2024-11-30 23:11:42 +0100

    Add shorten transformation

--- src/main/java/pl/put/poznan/transformer/logic/ShortenTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/ShortenTransformer.java
@@ -11,8 +11,29 @@ public class ShortenTransformer implements TextTransformer {
     @Override
     public String transform(String text) {
         String transformed = base.transform(text);
-        // tutaj logika
-        return transformed;
+        String[] words = transformed.split(" ");
+        StringBuilder shorten = new StringBuilder();
+        boolean upper;
+        for (String word : words) {
+            upper = Character.isUpperCase(word.charAt(0));
+            switch (word.toLowerCase()){
+                case "na przykład":
+                    shorten.append(upper ? "Np." : "np");
+                    break;
+                case "między innymi":
+                    shorten.append(upper ? "M.in." : "m.in.");
+                    break;
+                case "i tym podobne":
+                    shorten.append(upper ? "Itp." : "itp.");
+                    break;
+                default:
+                    shorten.append(word);
+                    break;
+            }
+            shorten.append(" ");
+        }
+
+        return shorten.toString().trim();
     }
 
 }
