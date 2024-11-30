138568667a8f52cbbdb2e611899539c3ace902ad
KcprZtn <kcprzaton@gmail.com>
2024-11-30 22:34:23 +0100

    Fix capitalize transformation

--- src/main/java/pl/put/poznan/transformer/logic/CapitalizeTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/CapitalizeTransformer.java
@@ -10,7 +10,18 @@ public class CapitalizeTransformer implements TextTransformer {
 
     @Override
     public String transform(String text) {
-        return base.transform(text).substring(0, 1).toUpperCase() + text.substring(1);
+        String [] words = base.transform(text).split(" ");
+        StringBuilder capitalized = new StringBuilder();
+
+        for (String word : words) {
+            if(!word.isEmpty()) {
+                capitalized.append(Character.toUpperCase(word.charAt(0)))
+                        .append(word.substring(1))
+                        .append(" ");
+            }
+
+        }
+        return capitalized.toString().trim();
     }
 
 }
