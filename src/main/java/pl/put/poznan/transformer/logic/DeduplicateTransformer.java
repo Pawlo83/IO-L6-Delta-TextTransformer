53f5773d2866dff00511dd9f68ca63f267c27c21
szymont20 <147140577+szymont20@users.noreply.github.com>
2024-12-08 19:40:38 +0100

    Update DeduplicateTransformer.java
    Dokumentacja [Javadoc]

--- src/main/java/pl/put/poznan/transformer/logic/DeduplicateTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/DeduplicateTransformer.java
@@ -1,13 +1,36 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Transformator usuwający powtarzające się wyrazy w bezpośrednim sąsiedztwie tekstu.
+ * <p>
+ * Działa poprzez analizę przekształconego tekstu i eliminację duplikatów wyrazów.
+ * </p>
+ * 
+ * @see TextTransformer
+ * @since 1.0
+ */
 public class DeduplicateTransformer implements TextTransformer {
 
+    /**
+     * Podstawowy transformator, na którym opiera się ten transformator.
+     */
     private final TextTransformer base;
 
+    /**
+     * Konstruktor klasy {@code DeduplicateTransformer}.
+     *
+     * @param base Podstawowy transformator tekstu.
+     */
     public DeduplicateTransformer(TextTransformer base) {
         this.base = base;
     }
 
+    /**
+     * Przekształca tekst, usuwając powtarzające się wyrazy w bezpośrednim sąsiedztwie.
+     *
+     * @param text Tekst do przetworzenia.
+     * @return Tekst po usunięciu duplikatów wyrazów.
+     */
     @Override
     public String transform(String text) {
         String transformed = base.transform(text);
