0cf4a755f15eecd1f839f280dce5040145cb6682
szymont20 <147140577+szymont20@users.noreply.github.com>
2024-12-09 18:44:28 +0100

    Update TextTransformer.java

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -1,5 +1,23 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Interfejs definiujący kontrakt dla transformatorów tekstu.
+ * Implementacje tego interfejsu przekształcają podany tekst zgodnie z określoną logiką.
+ * 
+ * <p>Przykładowe implementacje obejmują zmienianie wielkości liter, 
+ * rozszerzanie skrótów, odwracanie tekstu itp.</p>
+ * 
+ * @author  
+ * @version 1.0
+ * @since   1.0
+ */
 public interface TextTransformer {
+    
+    /**
+     * Przekształca podany tekst zgodnie z implementowaną logiką.
+     *
+     * @param text Tekst do przekształcenia.
+     * @return Przekształcony tekst.
+     */
     String transform(String text);
 }
